a882d83fe5be3871d57368543e7549c6b2d743ec
Jake Hoffman <jaketh19@gmail.com>
2020-08-09 20:43:29 -0400

    added Readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pi Collisions
+
+A physics simulation which features two blocks colliding. The number of collisions between the two blocks is equal to Pi when the ratio of the masses of the blocks is a power of one-hundred.
--- src/CollisionsMain.java
+++ src/CollisionsMain.java
@@ -1,8 +1,8 @@
 public class CollisionsMain {
 
     public static void main (String[] args){
-        //1000000000000000000
-        Blocks blocks  = new Blocks(1,67.0,500,700);
+        //Max Value: 1000000000000000000
+        Blocks blocks  = new Blocks(1,10000,200,700);
 
         new DisplayCollisions(1000, 500, blocks);
     }
